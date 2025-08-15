fe3f8fe5a56c9458d1f67360a242ac3f69ed6799
safi-is-coding <cse.mohammadsafi@gmail.com>
2025-08-15 20:51:51 +0530

    `Added Tailwind CSS and DaisyUI to the project, updated dependencies and configurations.`

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,24 @@ function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <div className="navbar bg-base-300 shadow-sm">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">DevTinder</a>
+        </div>
+        <div className="flex gap-2">
+        
+          <div className="dropdown dropdown-end mx-5">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Profile"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </>
   )
 }
